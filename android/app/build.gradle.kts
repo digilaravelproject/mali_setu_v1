96d750e314ae466520bb2a5b812cc745287707e4
Firoz Mohammad <firozm613@gmail.com>
2026-05-02 17:53:25 +0530

    Rename package to com.malisetu.app and update google-services.json

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.mali.setu"
+    namespace = "com.malisetu.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -45,7 +45,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.mali.setu"
+        applicationId = "com.malisetu.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
